33144a1378855a2bf2aabbb7e37141933895032c
Marco Rossini <marco.rossini@protonmail.ch>
2023-08-20 10:57:49 +0200

    Added forach and enumerate iteration macros.

--- iteration.lisp
+++ iteration.lisp
@@ -1,12 +1,42 @@
 (in-package :utils)
 
 (with-export
-  (defmacro dolist-idx ((item index list &optional result) &body body)
+  (defmacro with-escape (escape-symbol &body body)
+    "Defines a block and a symbol macro to escape from the block"
+    (with-gensyms (tag)
+      `(symbol-macrolet ((,escape-symbol (return-from ,tag)))
+         (block ,tag
+           ,@body))))
+
+  (defmacro foreach ((item) sequence &body body)
+    "Iterates over each sequence item and executes the given code."
+    `(with-escape break
+       (map nil (lambda (,item) (with-escape continue ,@body)) ,sequence)))
+
+  (defmacro enumerate ((index item) sequence &body body)
     "Iterates over the given list but also provides a list index along with the list item."
-    `(let ((,index 0))
-       (dolist (,item ,list ,result)
-         ,@body
-         (incf ,index))))
+    (with-gensyms (idx)
+      `(let ((,idx -1))
+         (foreach (,item) ,sequence
+           (incf ,idx)
+           (let ((,index ,idx))
+             ,@body))
+         ,idx)))
+
+  (defmacro enumerate-unpack ((index &rest items) sequence &body body)
+    (with-gensyms (item)
+      `(enumerate (,index ,item) ,sequence
+         (sbind ,items ,item
+           ,@body))))
+
+  (defmacro enumerate-zip ((index &rest items) (&rest sequences) &body body)
+    (when (/= (length items) (length sequences))
+      (error "Error expanding enumerate-zip: number of items does not match number of sequences."))
+    (with-gensyms (item)
+      `(enumerate (,index ,item) (map 'list #'vector ,@sequences)
+         (declare (ignorable ,index))
+         (let ,(loop for var in items for i upfrom 0 collect `(,var (elt ,item ,i)))
+           ,@body))))
 
   (defmacro until (condition &body body)
     `(do () (,condition)
